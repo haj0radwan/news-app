9e47763715bcae0988d3058b5f668fdd48304c26
haj0radwan <mrahmeed487@gmail.com>
2024-09-07 19:09:26 +0300

    newp in

--- lib/features/news/presentation/widgets/articleItem copy.dart
+++ lib/features/news/presentation/widgets/articleItem copy.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:news_app/features/news/data/models/articldto.dart';
 import 'package:news_app/features/news/presentation/bloc/news_bloc.dart';
-import '../../domain/entities/newsentity.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class ArticleItemDto extends StatelessWidget {
